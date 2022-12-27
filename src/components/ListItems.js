242566ece7ee36cf9e9b742c63b30e179584df2b
gagneja1512 <aadityaarora1215@gmail.com>
2022-12-28 00:13:11 +0530

    Adding Delete list Option

--- src/components/ListItems.js
+++ src/components/ListItems.js
@@ -8,16 +8,23 @@ const ListItems = (props) => {
     const dispatch = useDispatch()
     const getIdHandler = () => {
         // console.log(props.id)
-       dispatch(listActions.select({
-            id : props.id ,
-       }))
+        dispatch(listActions.select({
+            id: props.id,
+        }))
+    }
+
+    const deleteListHandler = () => {
+        dispatch(listActions.removeItemFromLists(props.id))
     }
 
 
     return (
         <Fragment>
             <div className={classes.listItems}>
-                <button onClick={getIdHandler} className={classes.btn_list}><div className={classes.square}></div><span>{props.title}</span></button>
+                <div>
+                    <button onClick={getIdHandler} className={classes.btn_list}><div className={classes.square}></div><span>{props.title}</span></button>
+                    <button className={classes.btndelete} onClick={deleteListHandler}>delete</button>
+                </div>
                 <span className={classes.taskLength}>{props.taskLength}</span>
             </div>
         </Fragment>
